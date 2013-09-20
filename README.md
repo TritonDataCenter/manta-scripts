954e5e0f9ee11375144db6502c13ec8d72bca199
Nate Fitch <nate.fitch@joyent.com>
2013-09-20 03:12:13 +0000

    MANTA-1731: manta deployment needs to cross the illumos#3942 flag day

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 These scripts are used for configuring manta zones and uploading log files.
+
+Note that this repository is used as a git submodule, and once updated, all
+repos that have this as a dependency need to be updated or *your changes will
+not get picked up*.  To update, run the update script like so:
+
+    [manta-scripts]$ ./update/repos.sh <Jira Item>
+
+In order for that script to work, you must have all of the manta repos cloned
+at ../.  The full list can be found in the repos.sh script.  If there are
+problems during updates, the script can safely be rerun.
---
+++ update/repos.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+###############################################################################
+# This script updates all the $REPOS to the latest verion of manta-scripts.
+# It will only update repos that:
+#   - Exist at ../[repo]
+#   - Don't have current, outstanding changes
+#
+# It will only update the repos that are out of date, so if there are problems
+# with repos, you can fix and safely rerun.
+###############################################################################
+
+REPOS=(
+    muppet
+    muskie
+    mahi
+    electric-moray
+    moray
+    manatee
+    mako
+    marlin
+    binder
+    medusa
+    wrasse
+    mola
+    madtom
+)
+PROBLEMS=( )
+DEP_LOC="deps/manta-scripts"
+
+if [ -z "$1" ]; then
+    echo "usage: $0 [Jira item for commits]"
+    exit 1
+fi
+
+JIRA=$1
+P=$PWD
+if [ $(basename $P) != "manta-scripts" ]; then
+    echo "Script must be run from manta-scripts directory"
+    exit 1
+fi
+MS_GIT_SHA=$(git rev-parse HEAD)
+MS_GIT_SHA_SHORT=$(git rev-parse --short HEAD)
+
+for repo in "${REPOS[@]}"; do
+    # Reset to manta-scripts directory each time
+    cd $P
+
+    if [ ! -d "../$repo" ]; then
+        PROBLEMS=( "${PROBLEMS[@]}" "$repo" )
+        echo "$repo doesn't exist at ../$repo.  Not updating."
+        continue
+    fi
+
+    echo "Checking $repo..."
+    cd "../$repo"
+    git pull --rebase
+    if [ $? != 0 ]; then
+        PROBLEMS=( "${PROBLEMS[@]}" "$repo" )
+        echo "Unable to 'git pull --rebase' $repo. Not updating."
+        continue
+    fi
+
+    REPO_GIT_SHA=$(git submodule $DEP_LOC | cut -c 2-41)
+    if [ "$MS_GIT_SHA" == "$REPO_GIT_SHA" ]; then
+        echo "$repo already has latest manta-scripts.  Not updating."
+        continue
+    fi
+
+    echo "Updating $repo..."
+    git submodule init $DEP_LOC
+    git submodule update $DEP_LOC
+    cd $DEP_LOC
+    git merge origin/master
+    cd -
+    git add $DEP_LOC
+    git commit -m "$JIRA: Updating to latest manta-scripts ($MS_GIT_SHA_SHORT)"
+    git push origin master
+    echo "Done updating $repo."
+
+done
+
+if [ ${#PROBLEMS[*]} != 0 ]; then
+    echo ""
+    echo "There were problems updating the following repos:"
+    echo "${PROBLEMS[@]}"
+else
+    echo "All repos up to date."
+fi
+
+#Leave you where I found you
+cd $P
